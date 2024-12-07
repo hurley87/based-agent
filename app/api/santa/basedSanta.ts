e00beaa64ae5cb1a1d5f6d7295f721cdb9ab847d
David Hurley <dhurls99@gmail.com>
2024-12-07 07:16:31 -0500

    check if user has received a present or not

---
+++ app/api/santa/basedSanta.ts
@@ -0,0 +1,289 @@
+export const BASED_SANTA_ADDRESS = '0x13A1AdfC1ea3c546bAB0f30d225Cd4eAFf7dE015';
+
+export const BASED_SANTA_ABI = [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnableInvalidOwner",
+      "type": "error"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "OwnableUnauthorizedAccount",
+      "type": "error"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "presentId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "tokenAddress",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amountOrTokenId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "bool",
+          "name": "isERC20",
+          "type": "bool"
+        }
+      ],
+      "name": "PresentCreated",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "presentId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        }
+      ],
+      "name": "PresentSent",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "tokenAddress",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        }
+      ],
+      "name": "createERC20Present",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "tokenAddress",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        }
+      ],
+      "name": "createERC721Present",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getUnsentPresentsCount",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "hasReceivedPresent",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "nextPresentIndex",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "presentCounter",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "presents",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "tokenAddress",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amountOrTokenId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bool",
+          "name": "isERC20",
+          "type": "bool"
+        },
+        {
+          "internalType": "bool",
+          "name": "sent",
+          "type": "bool"
+        },
+        {
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        }
+      ],
+      "name": "sendNextPresent",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ] as const;
--- app/api/santa/route.ts
+++ app/api/santa/route.ts
@@ -1,20 +1,30 @@
 import { createOpenAI } from '@ai-sdk/openai';
 import { generateText } from 'ai';
+import { BASED_SANTA_ADDRESS, BASED_SANTA_ABI } from './basedSanta';
+import { baseSepolia } from 'viem/chains';
+import { createWalletClient, http } from 'viem';
+import { createPublicClient } from 'viem';
+const RPC_URL = 'https://sepolia.base.org';
+
+const publicClient = createPublicClient({
+    chain: baseSepolia,
+    transport: http(RPC_URL),
+});
+  
+const walletClient = createWalletClient({
+    chain: baseSepolia,
+    transport: http(RPC_URL),
+});
+
 
 export async function POST(request: Request) {
     const req = await request.json();
-    console.log('req', req);
-
     const { OPENAI_API_KEY } = process.env;
-
     const castText = req.data.text;
-    console.log('castText', castText);
     const verifiedAddresses = req.data.author.verified_addresses;
-    console.log('verifiedAddresses', verifiedAddresses);
     const verifiedAddress =
       verifiedAddresses?.eth_addresses?.[0] ||
       ('0xbD78783a26252bAf756e22f0DE764dfDcDa7733c' as `0x${string}`);
-    console.log('verifiedAddress', verifiedAddress);
 
     if(!verifiedAddress) {
         return Response.json(
@@ -27,6 +37,26 @@ export async function POST(request: Request) {
 
     // check if verifiedAddress has received a present or not
     // prompt that they have received a present
+    const hasReceivedPresent = (await publicClient.readContract({
+        abi: BASED_SANTA_ABI,
+        address: BASED_SANTA_ADDRESS,
+        functionName: 'hasReceivedPresent',
+        args: [verifiedAddress],
+      })) as boolean;
+
+    console.log('hasReceivedPresent', hasReceivedPresent);
+
+    if(!hasReceivedPresent) {
+        return Response.json(
+            {
+                text: 'You have not received a present'
+            },
+            { status: 200 }
+        );
+    }
+
+
+
 
     // check that they have a balance of 1M Based
     // prompt that they have a balance of 1M Base
@@ -36,35 +66,35 @@ export async function POST(request: Request) {
 
     // write to 
 
-    const openai = createOpenAI({
-        baseURL: "https://api.openai.com/v1",
-        apiKey: OPENAI_API_KEY,
-    });
+    // const openai = createOpenAI({
+    //     baseURL: "https://api.openai.com/v1",
+    //     apiKey: OPENAI_API_KEY,
+    // });
 
-    const result = await generateText({
-    model: openai('gpt-4-turbo'),
-    messages: [
-        {
-        role: 'user',
-        content: [
-            { type: 'text', text: 'You are a JavaScript expert.' },
-            {
-            type: 'text',
-            text: `Error message: 403`,
-            },
-            { type: 'text', text: 'Explain the error message.' },
-        ],
-        },
-    ],
-    });
+    // const result = await generateText({
+    // model: openai('gpt-4-turbo'),
+    // messages: [
+    //     {
+    //     role: 'user',
+    //     content: [
+    //         { type: 'text', text: 'You are a JavaScript expert.' },
+    //         {
+    //         type: 'text',
+    //         text: `Error message: 403`,
+    //         },
+    //         { type: 'text', text: 'Explain the error message.' },
+    //     ],
+    //     },
+    // ],
+    // });
 
-    const text = result.text;
+    // const text = result.text;
 
-    console.log('text', text);
+    // console.log('text', text);
 
     return Response.json(
     {
-        text
+        success: true,
     },
     { status: 200 }
     );
