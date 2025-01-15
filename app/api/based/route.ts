058cbc969b4b23390c5265c9a697369d093700b8
David Hurley <dhurls99@gmail.com>
2025-01-15 15:34:09 -0500

    initialize coinbase api key

--- app/api/based/route.ts
+++ app/api/based/route.ts
@@ -1,9 +1,21 @@
 import { CdpAgentkit } from "@coinbase/cdp-agentkit-core";
 import { CdpToolkit } from "@coinbase/cdp-langchain";
+import { Coinbase } from "@coinbase/coinbase-sdk";
 
 export async function POST(request: Request) {
   const req = await request.json();
   console.log("req", req);
+  const { API_KEY_NAME, API_KEY_PRIVATE_KEY } = process.env;
+
+  if (!API_KEY_NAME || !API_KEY_PRIVATE_KEY) {
+    return Response.json({ message: "Environment variables are not set" }, { status: 500 });
+  }
+
+  new Coinbase({
+    apiKeyName: API_KEY_NAME as string,
+    privateKey: API_KEY_PRIVATE_KEY.replaceAll("\\n", "\n") as string,
+  });
+  
   // Configure CDP AgentKit
   const config = {
     cdpWalletData: process.env.WALLET_DATA || undefined,
